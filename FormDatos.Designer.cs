3d979ce1137b03efb93af8767a35bc8903ff43fc
pankeis123 <loro090626hgtpmla8@cbtis217.edu.mx>
2025-12-15 17:27:20 -0600

    Login hechoowoajdaoidao a

---
+++ FormDatos.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: EliteBook 840 G7
+ * Fecha: 29/11/2025
+ * Hora: 09:12 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace tarea
+{
+	partial class FormDatos
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// FormDatos
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "FormDatos";
+			this.Name = "FormDatos";
+		}
+	}
+}
